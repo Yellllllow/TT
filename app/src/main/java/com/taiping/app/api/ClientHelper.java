c8c360d71e8930cbf328c2deb478ec17c52b4e92
zhoujy <zjywangyi@163.com>
2017-05-05 10:49:27 +0800

    test newbranch1 by zhoujiy........

--- app/src/main/java/com/taiping/app/api/ClientHelper.java
+++ app/src/main/java/com/taiping/app/api/ClientHelper.java
@@ -36,7 +36,8 @@ import okhttp3.logging.HttpLoggingInterceptor;
 public class ClientHelper {
     private static volatile long serverTime;
     //test newbranch by zhoujiy
-
+    //test newbranch1 by zhoujiy
+    //test newbranch1 by zhoujiy.......
     public static long getServerTime() {
         serverTime = System.currentTimeMillis();
         return serverTime;
